63dc6ebe4721b407d6c260a6e45bd24ad5beb161
Douglas Meredith <noisyflowers@douglasmeredith.net>
2021-12-16 15:54:29 -0700

    Remove Person delete altogether. Add Reference Delete. Generalized relationship check routine.

--- DeletionResolvers.js
+++ DeletionResolvers.js
@@ -1,12 +1,13 @@
 //import * as neo4j from 'neo4j-driver';
 
-const hasAuthoredBys = async (session, pbotID) => {
-    let result
-    
+const hasRelationships = async (session, pbotID, relationship) => {
+    let result;
     result = await session.run(
             `
             MATCH 
-                (:Person {pbotID: $pbotID})<-[ab:AUTHORED_BY]-()
+                (n) WHERE n.pbotID = $pbotID
+            WITH n
+                MATCH (n)-[ab:${relationship}]-()
             RETURN
                 ab
             `,
@@ -23,45 +24,47 @@ const hasAuthoredBys = async (session, pbotID) => {
 
 export const DeletionResolvers = {
     Mutation: {
-        CustomDeletePerson: async (obj, args, context, info) => {
+        CustomDeleteReference: async (obj, args, context, info) => {
             const driver = context.driver;
             const session = driver.session()
             
             console.log("args");
             console.log(args);
             
-            if (await hasAuthoredBys(session, args.data.pbotID)) {
+            if (await hasRelationships(session, args.data.pbotID, "CITED_BY")) {
+                console.log("cannot delete");
                 return {pbotID: "Cannot delete " + args.data.pbotID}
             } else {
+                console.log("can delete");
                 //return {pbotID: "Can delete " + args.data.pbotID}
                 const result = await session.run(
                     `
                     MATCH 
-                        (person:Person {pbotID: $data.pbotID}),
-                        (ePerson:Person {pbotID: $data.enteredByPersonID})
-                    WITH person, ePerson					
+                        (reference:Reference {pbotID: $pbotID}),
+                        (ePerson:Person {pbotID: $enteredByPersonID})
+                    WITH reference, ePerson					
                         CREATE
-                            (person)-[:ENTERED_BY {timestamp: datetime(), type:"DELETE"}]->(ePerson)
-                    WITH person	
-                        REMOVE person:Person SET person:_Person
-                    WITH person
-                        OPTIONAL MATCH (person)<-[enteredBy:ENTERED_BY]-(node)
+                            (reference)-[:ENTERED_BY {timestamp: datetime(), type:"DELETE"}]->(ePerson)
+                    WITH reference	
+                        REMOVE reference:Reference SET reference:_Reference
+                    WITH reference
+                        OPTIONAL MATCH (reference)-[authoredBy:AUTHORED_BY]->(node)
                         CALL apoc.do.when(
-                            enteredBy IS NOT NULL,
-                            "CREATE (person)<-[archivedEnteredBy:_ENTERED_BY]-(node) SET archivedEnteredBy = enteredBy DELETE enteredBy RETURN person",
-                            "RETURN person",
-                            {person: person, node: node, enteredBy: enteredBy}
+                            authoredBy IS NOT NULL,
+                            "CREATE (reference)-[archivedAuthoredBy:_AUTHORED_BY]->(node) SET archivedAuthoredBy = authoredBy DELETE authoredBy RETURN reference",
+                            "RETURN reference",
+                            {reference: reference, node: node, authoredBy: authoredBy}
                         ) YIELD value
-                    WITH distinct value.person AS person 
+                    WITH distinct value.reference AS reference 
                     RETURN {
-                        pbotID: person.pbotID + " deleted"
-                    }
+                        pbotID: reference.pbotID + " deleted"
+                    } 
                     `,
                     {pbotID: args.data.pbotID, enteredByPersonID: args.data.enteredByPersonID}
                 );
                 console.log("result");
                 console.log(result);
-                return result.records[0];
+                return result.records[0]._fields[0];
             }
             
         }
