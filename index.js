67fe9e070232d4ea086dc966b8f8058b85aaffb5
Douglas Meredith <noisyflowers@douglasmeredith.net>
2022-06-21 14:46:36 -0700

    Hoooboy, chasing my tail on cypherMatchPostfix. The previous approach blocked access to elements of public groups under some circumstances that I don't completely understand. Let's try this one.

--- index.js
+++ index.js
@@ -163,8 +163,8 @@ const server = new ApolloServer({
                 schema,
                 cypherParams: {
                     user: user,
-                    cypherMatchPrefix: `(public:Group {name:"public"}),(p:Person {pbotID:"${user.pbotID}"})-[:MEMBER_OF]->(g:Group)<-[:ELEMENT_OF|:MEMBER_OF]-`,
-                    cypherMatchPostfix: `-[:ELEMENT_OF|:MEMBER_OF]->(group)<-[:MEMBER_OF]-(p) WHERE group IN [g,public]`,
+                    cypherMatchPrefix: `(p:Person {pbotID:"${user.pbotID}"})-[:MEMBER_OF]->(g:Group)<-[:ELEMENT_OF|:MEMBER_OF]-`,
+                    cypherMatchPostfix: `-[:ELEMENT_OF|:MEMBER_OF]->()<-[:MEMBER_OF]-(p)`,
                     skipPrefixNodeTypes: ["Person"]
                 },
             };
