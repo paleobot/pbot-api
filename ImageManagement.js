1ededa08f4702da321555825a6e9497c4d20cd46
Douglas Meredith <noisyflowers@douglasmeredith.net>
2022-09-02 11:35:45 -0700

    Added authorization check and other small improvements.

--- ImageManagement.js
+++ ImageManagement.js
@@ -4,43 +4,74 @@ import path from 'path'
 import jwt from 'jsonwebtoken';
 import ejwt from 'express-jwt';
 import bcrypt from 'bcrypt';
+import {getUser} from './UserManagement.js';
+
+const imageDir = '../../images'; //TODO: get from config
 
 const handleImages = async (req, res, driver) => {
     console.log("handleImages");
     
-    /*
-    //TODO: what about public specimens? Might need to pull pbotID from url, then check group 
-    // Get the user token from the headers.
+    //Make sure user is authorized for this image
     const token = req.headers.authorization;
     console.log(token);
-    if (!token) {
+
+    let authorized = false;
+    let email;
+    try {
+        if (token) {
+            const decodedToken = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
+            //console.log(decodedToken);
+            email = decodedToken.username;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    const user = await getUser(driver, email);
+    console.log("user.pbotID");
+    console.log(user.pbotID);
+    const session = driver.session();
+    await session.run(`
+        MATCH 
+            (person:Person {pbotID: $userID})-[:MEMBER_OF]->(:Group)<-[:ELEMENT_OF]-(s:Specimen {pbotID: $specimenID})
+        RETURN 
+            person      
+        `, {
+            userID: user.pbotID,
+            specimenID: req.params.pbotID
+        }
+    )
+    .then(result => {
+        console.log("cypher success");
+        let person;
+        result.records.forEach(record => {
+            person = record.get('person');
+        })
+        if (person) {
+            authorized = true;
+        }
+    })
+    .finally(() => {
+        console.log("closing session");
+        session.close();
+    });
+    
+    if (!authorized) {
         return res.status(401).send({
             code: 401, 
             msg: "Not authorized to access this resource",
         });
-    }
-    */
-    
-    if (req.method === "GET") {
-        console.log("reset, GET");
-        console.log(process.cwd());
-        /*
-        return res.status(501).send({
-            code: 501, 
-            msg: "GET not implemented",
-        });
-        */
-        //parse url to get pbotID as directory name
-        const parts = req.path.split('/');
-        console.log(parts);
-        res.sendFile(path.resolve('../../images/' + parts[2] + '/' + parts[3])); // put your app.html's         //res.sendFile(path.resolve('../../images/STScI-01GA6KNV1S3TP2JBPCDT8G826T.png')); // put your app.html's relative path        
     } else {
-        console.log("reset, POST");
-        return res.status(501).send({
-            code: 501, 
-            msg: "POST not implemented",
-        });
-    }    
+        if (req.method === "GET") {
+            console.log("image, GET");
+            res.sendFile(path.resolve(imageDir, req.params.pbotID, req.params.image)); 
+        } else {
+            console.log("image, POST");
+            return res.status(501).send({
+                code: 501, 
+                msg: "POST not implemented",
+            });
+        }  
+    }
 }
 
 export {handleImages}
