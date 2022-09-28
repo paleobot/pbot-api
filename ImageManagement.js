e669a874eefbfb5aa43204cd47969e26491fd1fe
Douglas Meredith <noisyflowers@douglasmeredith.net>
2022-09-28 13:35:19 -0700

    Splitting some pieces out of Resolvers.js

--- ImageManagement.js
+++ ImageManagement.js
@@ -4,11 +4,17 @@ import path from 'path'
 import jwt from 'jsonwebtoken';
 import ejwt from 'express-jwt';
 import bcrypt from 'bcrypt';
+import { promises as streamPromises } from 'stream';
 import {getUser} from './UserManagement.js';
 
-const imageDir = '../../images'; //TODO: get from config
+import dotenv from "dotenv"
 
-const handleImages = async (req, res, driver) => {
+dotenv.config();
+
+const imageDir = process.env.IMAGE_DIR;
+const imageLinkPre = process.env.IMAGE_LINK_PRE;
+
+export const handleImages = async (req, res, driver) => {
     console.log("handleImages");
     
     //Make sure user is authorized for this image
@@ -27,6 +33,8 @@ const handleImages = async (req, res, driver) => {
         console.log(error);
     }
     const user = await getUser(driver, email);
+    console.log("email");
+    console.log(email)
     console.log("user.pbotID");
     console.log(user.pbotID);
     const session = driver.session();
@@ -74,4 +82,101 @@ const handleImages = async (req, res, driver) => {
     }
 }
 
-export {handleImages}
+export const uploadFile = async ( file, imagedID ) => {
+    console.log("uploadFile");
+    
+    try {
+        const { createReadStream, filename, mimetype, encoding } = await file;
+        
+        // Invoking the `createReadStream` will return a Readable Stream.
+        // See https://nodejs.org/api/stream.html#stream_readable_streams
+        const stream = createReadStream();
+
+        if (!fs.existsSync(path.resolve(imageDir, imagedID))){
+            fs.mkdirSync(path.resolve(imageDir, imagedID), { recursive: true });
+        }
+        
+        //const newFilename = `${crypto.randomUUID()}--${filename}`;
+        const newFilename = filename; //TODO: Not worrying about collisions for now. This makes Update easier.
+        
+        // TODO: get pbotID and imageDir
+        const filePath = path.resolve(imageDir, imagedID, newFilename);
+        
+        const out = fs.createWriteStream(filePath);
+        stream.pipe(out);
+        await streamPromises.finished(out);
+
+        //TODO: build this from stuff
+        return {
+            path: filePath,
+            link: imageLinkPre + "/" + imagedID + "/" + newFilename
+        
+        };
+    } catch (error) {
+        throw new Error(`Unable to upload file: ${error.message}`);
+    }
+    
+}
+        
+export const renameFile = async ( context, pbotID, revert ) => {
+    console.log("---------------rename file--------------------");
+    console.log(pbotID);
+    
+    const driver = context.driver;
+    const session = driver.session()
+
+    const queryStr = `
+        MATCH 
+            (n)
+        WHERE
+            n.pbotID = "${pbotID}"
+        RETURN
+            n
+    `;
+    
+    const pgResult = await session.run(
+        queryStr
+    );
+    const link = pgResult.records.length > 0 ? pgResult.records[0].get(0).properties.link : null;    
+    console.log(link);
+    
+    const regex = /([^\/]*\/[^\/]+)$/g;    
+    const filePath = new URL(link).pathname.match(regex)[0];
+    const tmpFilePath = `${filePath}.sav`;
+    
+    let oldFullPath, newFullPath;
+    if (revert) {
+        newFullPath = path.join(imageDir, filePath);
+        oldFullPath = path.join(imageDir, tmpFilePath);
+    } else {
+        oldFullPath = path.join(imageDir, filePath);
+        newFullPath = path.join(imageDir, tmpFilePath);
+    }
+    
+    console.log(oldFullPath);
+    console.log(newFullPath);
+    
+    try {
+        await fs.renameSync(oldFullPath, newFullPath);
+    } catch (error) {
+        throw new Error(`Unable to rename file ${oldFullPath}: ${error.message}`);
+    }    
+    
+    return newFullPath;
+    
+}
+
+export const deleteFile = async ( filePath ) => {
+    console.log("---------------delete file--------------------");
+    console.log(filePath);
+        
+    try {
+        await fs.unlinkSync(filePath);
+    } catch (error) {
+        throw new Error(`Unable to delete file ${filePath}: ${error.message}`);
+    }    
+    
+    return `${filePath} deleted`;
+    
+}
+
---
+++ SchemaMaps.js
@@ -0,0 +1,684 @@
+export const schemaDeleteMap = {
+    Person: {
+        blockingRelationships: [{
+            type: "ENTERED_BY",
+            direction: "in"
+        }, {
+            type: "AUTHORED_BY",
+            direction: "in"
+        }],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "MEMBER_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }]
+    },
+    Group: {
+        blockingRelationships: [{
+            type: "MEMBER_OF",
+            direction: "in"
+        }, {
+            type: "ITEM_OF",
+            direction: "in"
+        }],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "ENTERED_BY",
+            direction: "out"
+        }]
+    }, 
+    Reference: {
+        blockingRelationships: [{
+            type: "CITED_BY",
+            direction: "out"
+        }],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "AUTHORED_BY",
+            direction: "out"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }]
+    }, 
+    Schema: {
+        blockingRelationships:  [{
+            type: "APPLICATION_OF",
+            direction: "in"
+        }],
+        cascadeRelationships: [{
+            type: "CHARACTER_OF",
+            direction: "in"
+        }],
+        nonblockingRelationships: [{
+            type: "AUTHORED_BY",
+            direction: "out"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }, {
+            type: "CITED_BY",
+            direction: "in"
+        }]
+    }, 
+    Character: {
+        blockingRelationships: [{
+            type: "INSTANCE_OF",
+            direction: "in"
+        }],
+        cascadeRelationships: [{
+            type: "CHARACTER_OF",
+            direction: "in"
+        }, {
+            type: "STATE_OF",
+            direction: "in"
+        }],
+        nonblockingRelationships: [{
+            type: "CHARACTER_OF",
+            direction: "out"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }]
+    },
+    State: {
+        blockingRelationships: [{
+            type: "HAS_STATE",
+            direction: "in"
+        }],
+        cascadeRelationships: [{
+            type: "STATE_OF",
+            direction: "in"
+        }],
+        nonblockingRelationships: [{
+            type: "STATE_OF",
+            direction: "out"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }]
+    },
+    Description: {
+        blockingRelationships: [],
+        cascadeRelationships: [{
+            type: "DEFINED_BY",
+            direction: "out"
+        }],
+        nonblockingRelationships: [{
+            type: "APPLICATION_OF",
+            direction: "out"
+        }, {
+            type: "DESCRIBED_BY",
+            direction: "in"
+        }, {
+            type: "EXAMPLE_OF",
+            direction: "in"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }, {
+            type: "CITED_BY",
+            direction: "in"
+        }]
+    },
+    CharacterInstance: {
+        blockingRelationships: [],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "CANDIDATE_FOR",
+            direction: "out"
+        }, {
+            type: "DEFINED_BY",
+            direction: "in"
+        }, {
+            type: "INSTANCE_OF",
+            direction: "out"
+        }, {
+            type: "HAS_STATE",
+            direction: "out"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }]
+    }, 
+    OTU: {
+        blockingRelationships: [{
+            type: "SAME_AS",
+            direction: "out"
+        }],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "EXAMPLE_OF",
+            direction: "out"
+        }, {
+            type: "HOLOTYPE_OF",
+            direction: "out",
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }, {
+            type: "CITED_BY",
+            direction: "in"
+        }]
+    },
+    Synonym: {
+        blockingRelationships: [],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "SAME_AS",
+            direction: "in"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }, {
+            type: "CITED_BY",
+            direction: "in"
+        }]
+    },
+    Comment: {
+        blockingRelationships: [{
+            type: "REFERS_TO",
+            direction: "in"
+        }],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "REFERS_TO",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }, {
+            type: "CITED_BY",
+            direction: "in"
+        }]
+    },
+    Specimen: {
+        blockingRelationships: [{
+            type: "DESCRIBED_BY",
+            direction: "out",
+        }, {
+            type: "HOLOTYPE_OF",
+            direction: "out",
+        }],
+        cascadeRelationships: [{
+            type: "IMAGE_OF",
+            direction: "in"
+        }], 
+        nonblockingRelationships: [{
+            type: "IS_TYPE",
+            direction: "out"
+        }, {
+            type: "EXAMPLE_OF",
+            direction: "out"
+        }, {
+            type: "COLLECTED_IN",
+            direction: "out"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }, {
+            type: "CITED_BY",
+            direction: "in"
+        }]
+    }, 
+    Image: {
+        blockingRelationships: [],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "IMAGE_OF",
+            direction: "out"
+        }, {
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }]
+    }, 
+    Collection: {
+        blockingRelationships: [{
+            type: "COLLECTED_IN",
+            direction: "in"
+        }, ],
+        cascadeRelationships: [],
+        nonblockingRelationships: [{
+            type: "ELEMENT_OF",
+            direction: "out"
+        }, {
+            type: "ENTERED_BY",
+            direction: "out"
+        }, {
+            type: "CITED_BY",
+            direction: "in"
+        }]
+    }, 
+}
+
+//In theory, the information contained in this map is already in the schema definion in schema.graphql.
+//However, accessing that information is a challenge. For instance, say we want a list of the properties 
+//allowed for a given node type. We have to do something like this:
+//      Object.keys(context.schema._typeMap.GroupInput._fields)
+//That's ugly. And it's not quite right. The resulting list will include properties we've mapped to 
+//Cypher relationships. We need to exclude those. Also, we don't really want pbotID or enteredByPersonID for our purposes, 
+//so we have to do this:
+//      Object.keys(context.schema._typeMap.GroupInput._fields).filter(property => !["pbotID", "enteredByPersonID"].includes(property) && !context.schema._typeMap.GroupInput._fields[property].type.ofType)
+//Beyond ugly.
+//And we also need to be able to get a list of relationships that includes both their Cypher name and their graphql name.
+//There is probably a way to get this from schema.graphql, but it's probably terrible.
+//So, I'm ok with the extra maintenance required by this map.
+//
+//It's important to note that the relationships listed here for each node type are only the relationships 
+//we want a mutation on that node type to have access to. For instance, the Specimen type below does not include 
+//EXAMPLE_OF and HOLOTYPE_OF because we don't want mutations on Specimen to modify these. They are only modified
+//by OTU.
+export const schemaMap = {
+    Group: {
+        properties: ["name"],
+        relationships: [
+            {
+                type: "MEMBER_OF",
+                direction: "in",
+                graphqlName: "members",
+                required: false,
+                updatable: true
+            }
+        ]
+    },
+    Person: {
+        properties: [
+            "given",
+            "surname",
+            "email",
+            "orcid"
+        ],
+        relationships:[
+            {
+                type: "MEMBER_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+            
+        ]
+    },
+    Reference: {
+        properties: [
+           "title",
+           "year",
+           "publisher",
+           "doi"
+        ],
+        relationships: [
+            {
+                type: "AUTHORED_BY",
+                direction: "out",
+                graphqlName: "authors",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order"
+                ]
+            },
+            {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    Schema: {
+        properties: [
+           "title",
+           "year",
+           "acknowledgments"
+        ],
+        relationships: [
+            {
+                type: "CITED_BY",
+                direction: "in",
+                graphqlName: "references",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            }, {
+                type: "AUTHORED_BY",
+                direction: "out",
+                graphqlName: "authors",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            },
+            {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    Character: {
+        properties: [
+           "name",
+           "definition"
+        ],
+        relationships: [
+            {
+                type: "CHARACTER_OF",
+                direction: "out",
+                graphqlName: "parentID",
+                required: true,
+                updatable: false
+            },
+            {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    State: {
+        properties: [
+            "name",
+            "definition"
+        ],
+        relationships: [
+            {
+                type: "STATE_OF",
+                direction: "out",
+                graphqlName: "parentID",
+                required: true,
+                updatable: true
+            },
+            {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    Description: {
+        properties: [
+			"name",
+        ],
+        relationships: [
+            {
+                type: "APPLICATION_OF",
+                direction: "out",
+                graphqlName: "schemaID",
+                required: true,
+                updatable: true
+            }, {
+                type: "DESCRIBED_BY",
+                direction: "in",
+                graphqlName: "specimenIDs",
+                required: false,
+                updatable: true
+            }, {
+                type: "CITED_BY",
+                direction: "in",
+                graphqlName: "references",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            }, {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    OTU: {
+        properties: [
+			"name",
+			"family",
+			"genus",
+			"species"
+        ],
+        relationships: [{
+                type: "EXAMPLE_OF",
+                direction: "in",
+                graphqlName: "exampleSpecimens",
+                required: true,
+                updatable: true
+            }, {
+                type: "HOLOTYPE_OF",
+                direction: "in",
+                graphqlName: "holotype",
+                required: true,
+                updatable: true
+            }, {
+                type: "SAME_AS",
+                direction: "out",
+                graphqlName: "synonyms",
+                required: false,
+                updatable: true
+            }, {
+                type: "CITED_BY",
+                direction: "in",
+                graphqlName: "references",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            }, {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    Synonym: {
+        properties: [
+            "explanation",
+        ],
+        relationships: [
+            {
+                type: "SAME_AS",
+                direction: "in",
+                graphqlName: "otus",
+                required: true,
+                updatable: true
+            }, {
+                type: "CITED_BY",
+                direction: "in",
+                graphqlName: "references",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            }, {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    Comment: {
+        properties: [
+            "content"
+        ],
+        relationships: [
+            {
+                type: "REFERS_TO",
+                direction: "out",
+                graphqlName: "subjectID",
+                required: true,
+                updatable: true
+            }, {
+                type: "CITED_BY",
+                direction: "in",
+                graphqlName: "references",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            }        
+        ]
+    },
+    Specimen: {
+        //TODO: look into https://www.graphql-scalars.dev/docs/scalars/uuid for managing idigbiouuid
+        properties: [
+           "name",
+           "preservationMode",
+           "idigbiouuid",
+           "pbdbcid",
+           "pbdboccid"
+        ],
+        relationships: [
+            {
+                type: "DESCRIBED_BY",
+                direction: "out",
+                graphqlName: "descriptionID",
+                required: false,
+                updatable: true,
+            }, {
+                type: "IS_TYPE",
+                direction: "out",
+                graphqlName: "organID",
+                required: true,
+                updatable: true
+            }, {
+                type: "COLLECTED_IN",
+                direction: "out",
+                graphqlName: "collection",
+                required: false,
+                updatable: true
+            }, {
+                type: "CITED_BY",
+                direction: "in",
+                graphqlName: "references",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            }, {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    Organ: {
+        properties: [
+           "type"
+        ],
+        relationships: []
+    },
+    Collection: {
+        properties: ["name"],
+        relationships: [
+            {
+                type: "CITED_BY",
+                direction: "in",
+                graphqlName: "references",
+                required: false,
+                updatable: true,
+                properties: [
+                    "pbotID",
+                    "order",
+                ]
+            }, {
+                type: "COLLECTED_IN",
+                direction: "in",
+                graphqlName: "specimens",
+                required: false,
+                updatable: true
+            }, {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: false,
+                updatable: true
+            }
+        ]
+    },
+    Image: {
+        properties: [
+           "link",
+           "citation",
+           "caption",
+           "type"
+        ],
+        relationships: [
+            {
+                type: "IMAGE_OF",
+                direction: "out",
+                graphqlName: "imageOf",
+                required: true,
+                updatable: true,
+            },
+            {
+                type: "ELEMENT_OF",
+                direction: "out",
+                graphqlName: "groups",
+                required: true,
+                updatable: true
+            }
+        ]
+    },
+    
+}
